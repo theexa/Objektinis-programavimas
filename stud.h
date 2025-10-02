95fe20b594f553313e1801f058a6342106e78d2b
theexa <jurgita.juch@gmail.com>
2025-10-02 23:23:25 +0300

    stud.h

---
+++ stud.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string var;
+    std::string pav;
+    std::vector<int> paz;
+    int egz;
+    double gal;
+    double med;
+    std::string gr;
+};
