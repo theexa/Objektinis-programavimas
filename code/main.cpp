ed45be7aa4d798910d3a4ed32774d07454b94ff7
theexa <jurgita.juch@gmail.com>
2025-11-07 09:18:11 +0200

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -33,20 +33,33 @@ int main() {
         StudentuSarG(n, m);
         fpav = "studentai.txt";
         cout << "Failas 'studentai.txt' sugeneruotas" << endl;
-    } else {
-        cout << "Įveskite failo pavadinimą: ";
-        cin >> fpav;
     }
     
     // Testavimo ciklas
     vector<double> visiLaikai;
+    
     if (konteinerioPasirinkimas == 1) {
         // Testavimas su vektoriumi
         cout << "\n=== TESTAVIMAS SU VEKTORIUMI ===" << endl;
+        vector<Studentas> studentai;
+        int pas;
+        cout << "Nuskaityti duomenis iš failo - (1), Ivesti duomenis ranka - (2): ";
+        cin >> pas;
+        if (pas == 1){
+            cout << "Įveskite failo pavadinimą: ";
+            cin >> fpav;
+            SkaitytiIsFailo(fpav, studentai);
+        } else{
+            int kiek;
+            cout << "Kiek studentu norite ivesti? ";
+            cin >> kiek;
+            for (int i = 0; i < kiek; i++) {
+                cout << "\nIvedamas " << i+1 << " studentas:" << endl;
+                RanksinisIvedimas(studentai);
+            }
+        }
         for (int i = 0; i < testuKiekis; i++) {
             cout << "\n--- Testas " << (i + 1) << " ---" << endl;
-            vector<Studentas> studentai;
-            SkaitytiIsFailo(fpav, studentai);
             
             double laikas = VykdytiStrategija<vector<Studentas>>(studentai, strategijosPasirinkimas);
             visiLaikai.push_back(laikas);
@@ -55,10 +68,25 @@ int main() {
     else if (konteinerioPasirinkimas == 2) {
         // Testavimas su sąrašu
         cout << "\n=== TESTAVIMAS SU SĄRAŠU ===" << endl;
+        list<Studentas> studentai;
+        int pas;
+        cout << "Nuskaityti duomenis iš failo - (1), Ivesti duomenis ranka - (2): ";
+        cin >> pas;
+        if (pas == 1){
+            cout << "Įveskite failo pavadinimą: ";
+            cin >> fpav;
+            SkaitytiIsFailo(fpav, studentai);
+        } else{
+            int kiek;
+            cout << "Kiek studentu norite ivesti? ";
+            cin >> kiek;
+            for (int i = 0; i < kiek; i++) {
+                cout << "\nIvedamas " << i+1 << " studentas:" << endl;
+                RanksinisIvedimas(studentai);
+            }
+        }
         for (int i = 0; i < testuKiekis; i++) {
             cout << "\n--- Testas " << (i + 1) << " ---" << endl;
-            list<Studentas> studentai;
-            SkaitytiIsFailo(fpav, studentai);
             
             double laikas = VykdytiStrategija<list<Studentas>>(studentai, strategijosPasirinkimas);
             visiLaikai.push_back(laikas);
@@ -77,7 +105,7 @@ int main() {
     }
     
 
-    
+    cout << "-----------------------------------" << endl;
     double suma = 0.0;
     for (int i = 0; i < testuKiekis; i++) {
         cout << "Testas " << (i + 1) << ": " << std::fixed << std::setprecision(6) << visiLaikai[i] << " s" << endl;
@@ -90,4 +118,3 @@ int main() {
     
     return 0;
 }
-
